453a2a8046410b8a1c2c9d87e6cc15ef58173882
SandeepUKale <40802978+SandeepUKale@users.noreply.github.com>
2019-08-27 18:47:47 +0530

    Updated service to TransactionService

--- TransactionService/src/main/java/com/mission/webservices/TransactionApplication.java
+++ TransactionService/src/main/java/com/mission/webservices/TransactionApplication.java
@@ -7,13 +7,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 import com.mission.webservices.model.Account;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TransactionApplication {
 
 //	@Autowired
 //	private Account account;
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext cac = SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext cac = SpringApplication.run(TransactionApplication.class, args);
 		Account account2 = cac.getBean(Account.class);
 		System.out.println(account2);
 
